f1c548a74f562a44159257e7c1821710d5795a3d
KavinPhan <phank@DESKTOP-V75L1PJ>
2018-02-04 16:36:33 -0500

    Changed AutonomousCommand to extend CommandGroup
    Makes sense since the autonomous command shouldn't be direct calls to
    the drivetrain subsystem, but instead a group of drive commands in
    sequential order

--- src/org/usfirst/frc/team6750/robot/commands/AutonomousCommand.java
+++ src/org/usfirst/frc/team6750/robot/commands/AutonomousCommand.java
@@ -2,7 +2,7 @@ package org.usfirst.frc.team6750.robot.commands;
 
 import edu.wpi.first.wpilibj.command.*;
 
-public class AutonomousCommand extends Command {
+public class AutonomousCommand extends CommandGroup {
 	public AutonomousCommand() {
 	}
 
